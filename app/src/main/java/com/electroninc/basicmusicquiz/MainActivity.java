d0b35041eed7e47487aeaca3ad665fe7e4326c56
Victor Durojaiye <durojaiyevic@gmail.com>
2020-06-02 00:27:26 +0100

    Implemented navigation to QuizActivity

--- app/src/main/java/com/electroninc/basicmusicquiz/MainActivity.java
+++ app/src/main/java/com/electroninc/basicmusicquiz/MainActivity.java
@@ -2,7 +2,10 @@ package com.electroninc.basicmusicquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startQuizButton = findViewById(R.id.start_quiz);
+        startQuizButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
